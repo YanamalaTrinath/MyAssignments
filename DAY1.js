14930beddd5be0e3b02acd33b270c1407c9bc29d
YanamalaTrinath <thrinathreddy05@gmail.com>
2025-09-18 19:10:27 +0530

    Add files via upload
    Day1

---
+++ DAY1.js
@@ -0,0 +1,16 @@
+
+let fristName = "Trinath";
+console.log( fristName)
+console.log(typeof fristName)
+
+let companyName = "Amazon";
+console.log(companyName)
+console.log(typeof companyName)
+
+let mobileNumber = 9949910661;
+console.log( mobileNumber)
+console.log(typeof mobileNumber)
+let isAutomation = true;
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
