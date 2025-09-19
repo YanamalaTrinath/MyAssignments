b4461fa03308507f85b5ee40266ed6ce563ac4a4
YanamalaTrinath <thrinathreddy05@gmail.com>
2025-09-19 19:13:26 +0530

    Add files via upload
    oddoreven

---
+++ oddorEven.js
@@ -0,0 +1,11 @@
+function isoddorEven(number){
+    if (number %2 ==0){
+        console.log("Number is Even")
+    }
+    else {
+        console.log("Number is Odd")
+
+    }
+    }
+    isoddorEven(6)
+    isoddorEven(561)
