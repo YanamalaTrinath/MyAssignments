03f8a584579590d865e16db69b34bde10235e730
YanamalaTrinath <thrinathreddy05@gmail.com>
2025-09-22 22:47:37 +0530

    Add files via upload

---
+++ Numbertypes.js
@@ -0,0 +1,17 @@
+function numbertype(num) 
+{
+    if (num > 2)   
+         {
+        console.log("The number is Positive");
+        }
+        else if (num < 5) {
+        console.log("The number is Negative");         
+        }
+        else 
+        {
+        console.log("The number is Netural");         
+
+        }
+}
+let num = '25';
+numbertype(num);
