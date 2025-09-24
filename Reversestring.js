7031489c4266a9aea8c22965bb387f5aaf67e230
YanamalaTrinath <thrinathreddy05@gmail.com>
2025-09-24 23:15:17 +0530

    Add files via upload

---
+++ Reversestring.js
@@ -0,0 +1,12 @@
+function reversestring(str) {
+    let reverse = "";
+
+    for (let i = str.length -1; i>= 0; i--) {
+        
+        reverse += str[i];
+        
+    }
+    return reverse;
+    
+}
+console.log(reversestring("T20 world cup"));
