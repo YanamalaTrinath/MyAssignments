359583e41628d38d0e670e85e9b33c6a05f17ace
YanamalaTrinath <thrinathreddy05@gmail.com>
2025-09-28 10:45:41 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,14 @@
+// let arrayvalue =["a","b","c"]
+
+// let arraynum =[1,2,3,"a"]
+
+const number =[2,4,5,2,1,2]
+let count = 0
+for (let index = 0; index < number.length; index++) {
+    if (number[index]==2) {
+        count ++;
+    }
+    
+}
+console.log(count);
+
